e98cc69475648d55932c6cf74e899d690bcfae31
Tommy <azran.thomas@gmail.com>
2025-05-29 15:38:13 -0400

    setting up create route

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import psycopg2
 import os 
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -16,17 +17,28 @@ conn = psycopg2.connect(
 conn.autocommit = True
 cur = conn.cursor()
 
-# Helper to load SQL files
-def load_sql(filename):
-    with open(os.path.join("db", "queries", filename), "r") as file:
-        return file.read()
+# Helper to load SQL file functions
+def load_sql(function_name):
+ with open(os.path.join("db", "queries", "users_queries", f"{function_name}.sql"), "r") as file:
+    return file.read()
 
 @app.route('/')
 def home():
     return '<h1>Flask REST API with PostgreSQL</h1>'
 
 # CREATE
-
+@app.route('/create', methods=['POST'])
+def create_user():
+  data = request.get_json()
+  hashed_password = generate_password_hash(data['hashed_password'])
+  query = load_sql('create_user_query')
+  cur.execute(query, (
+    data['name'],
+    data['email'],
+    hashed_password,
+  ))
+  return jsonify({"message": f"User '{data['name']}' created successfully"}), 201
+    
 
 # READ
 
